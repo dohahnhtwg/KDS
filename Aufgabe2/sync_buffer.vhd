e5e997d67e3cb8b6c9c5cf81e3e5dc0f8804fe34
edc09 <edc09@in.fhkn.de>
2016-11-17 10:27:55 +0100

    dff

--- Aufgabe2/sync_buffer.vhd
+++ Aufgabe2/sync_buffer.vhd
@@ -19,3 +19,35 @@ END sync_buffer;
 -- Im Rahmen der 2. Aufgabe soll hier die Architekturbeschreibung
 -- zur Entity sync_buffer implementiert werden.
 --
+architecture behavioral of sync_buffer is
+   signal dff1:  std_logic;            -- dff1 q+1
+   signal dff1_out:  std_logic;        -- dff1 q
+   signal dff2:  std_logic;            -- dff2 q+1
+   signal dff2_out:  std_logic;        -- dff2 q
+begin
+
+   -- dff1 Process
+   dff1_out <= dff1
+
+   process (rst, clk) is
+      begin
+         if rst=RSTDEF then
+            dff1 <= '0';
+         elsif rising_edge(clk) then
+            dff1 <= din;
+         end if;
+   end process;
+   
+   -- dff2 Process
+   dff2_out <= dff2
+
+   process (rst, clk) is
+      begin
+         if rst=RSTDEF then
+            dff2 <= '0';
+         elsif rising_edge(clk) then
+            dff2 <= dff1;
+         end if;
+   end process;
+
+end behavioral;
