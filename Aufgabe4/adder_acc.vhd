afe580124dbfa72eb8d9ab018a317a458a56284b
dohahn <dohahn@htwg-konstanz.de>
2017-01-25 21:38:25 +0100

    98

--- Aufgabe4/adder_acc.vhd
+++ Aufgabe4/adder_acc.vhd
@@ -15,33 +15,32 @@ END adder_acc;
 
 ARCHITECTURE structure OF adder_acc IS
   
-  CONSTANT ELE: std_logic_vector(4 DOWNTO 0) := "10000"; -- matrix size
-  SIGNAL cnt: std_logic_vector(4 DOWNTO 0);              -- intern counter
+  CONSTANT ELE: std_logic_vector(3 DOWNTO 0) := "1111"; -- matrix size
+  SIGNAL cnt: std_logic_vector(3 DOWNTO 0);              -- intern counter
   SIGNAL tmp: std_logic_vector(43 DOWNTO 0);
   SIGNAL mem_counter: unsigned(9 downto 0);              -- Actual adress of vector
   
 BEGIN
 
   PROCESS(rst, clk) IS
+    variable tmp: std_logic_vector(43 DOWNTO 0);
   BEGIN
     IF rst = RSTDEF THEN
-      tmp <= (OTHERS => '0');
-      cnt <= (OTHERS => '0');
-      mem_counter <= (OTHERS => '0');
+      tmp := (OTHERS => '0');
+      cnt <= (OTHERS => '1');
+      mem_counter <= (OTHERS => '1');
     ELSIF rising_edge(clk) THEN
       IF en = '1' THEN
         IF cnt=ELE THEN
-          tmp <= std_logic_vector(resize(unsigned(a), tmp'length));
-          cnt <= "00001";
-          mem_counter <= mem_counter + 1;
-        ELSE
-          tmp <= tmp + a;
-          cnt <= cnt + 1;
+          tmp := (OTHERS => '0');
+          mem_counter <= mem_counter + 1;        
         END IF;
+        tmp := tmp + a;
+        cnt <= cnt + 1;
+        p <= tmp;
       END IF;
     END IF;
   END PROCESS;
-  p <= tmp;
   addr <= std_logic_vector(mem_counter);
 
 END structure;
